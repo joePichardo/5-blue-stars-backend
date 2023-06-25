7e1008ecb59e4a81ac0a87dbd1b5d61b201b483b
joePichardo <joepichar@gmail.com>
2023-06-25 12:07:24 -0700

    setup typeorm with postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['src/**/*.entity.ts'],
+      synchronize: Boolean(process.env.DB_SYNC),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
